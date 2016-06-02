12d3606a3f1661f710dc50964985f32b11faa207
Jacob Baby <jbaby@manh.com>
2016-06-01 22:52:28 -0400

    add in some code

--- console/src/main/java/org/exp/ConsoleMain.java
+++ console/src/main/java/org/exp/ConsoleMain.java
@@ -1,7 +1,21 @@
 package org.exp;
 
+import java.util.Arrays;
+
+import org.apache.commons.lang.StringUtils;
+
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
+
 public class ConsoleMain {
 	public static void main(String[] args) {
-		System.out.println("Hi");
+		System.out.println(StringUtils.isEmpty("") ? "empty" : "not empty");
+		
+		Table<String, String, String> table = HashBasedTable.create();
+		table.put("X1", "Y1", "1");
+		
+		System.out.println(table.get("X1", "Y1"));
+		
+		//mvn exec:java -Dexec.mainClass="org.exp.ConsoleMain"
 	}
 }
